450fa5d7deb51ec3d06e79c83c18db878c8b0e24
syukrontanzilah <syukrontanzilah@gmail.com>
2024-12-04 05:52:57 +0700

    logo ticker

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,12 @@
 import ArrowIcon from '@/assets/arrow-right.svg';
-import CogImage from '@/assets/cog.png'
+import CogImage from '@/assets/cog.png';
+import CilinderImage from '@/assets/cylinder.png';
+import NoodleImage from '@/assets/noodle.png';
 import Image from 'next/image';
 
 export const Hero = () => {
   return (
-    <section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183ec2,#eaeefe_66%)]'>
+    <section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183ec2,#eaeefe_100%)] overflow-x-clip'>
       <div className="container">
         <div className='md:flex items-center'>
         {/* Title and description hero */}
@@ -23,7 +25,9 @@ export const Hero = () => {
 
         {/* Image Hero */}
         <div className='mt-20 md:mt-0 md:h-[648px] md:flex-1 relative'>
-          <Image src={CogImage} alt='cog' className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6'/>
+          <Image src={CogImage} alt='cog' className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0'/>
+          <Image src={CilinderImage} alt='cylinder' width={220} height={220} className='hidden md:block -top-8 -left-32 md:absolute'/>
+          <Image src={NoodleImage} alt='noodle' width={220} height={220} className='hidden md:block md:absolute top-[524px] left-[448px] rotate-[30deg]'/>
         </div>
           
         </div>
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,50 @@
+import AcmeLogo from "@/assets/logo-acme.png";
+import QuantumLogo from "@/assets/logo-quantum.png";
+import EchoLogo from "@/assets/logo-echo.png";
+import CelestialLogo from "@/assets/logo-celestial.png";
+import PulseLogo from "@/assets/logo-pulse.png";
+import ApexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image
+              src={AcmeLogo}
+              alt="logo-ticker"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={QuantumLogo}
+              alt="logo-ticker"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={EchoLogo}
+              alt="logo-ticker"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={CelestialLogo}
+              alt="logo-ticker"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={PulseLogo}
+              alt="logo-ticker"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={ApexLogo}
+              alt="logo-ticker"
+              className="logo-ticker-image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
